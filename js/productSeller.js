6a4272f447a6f976b0ff357eec8e1ae1fd4864b2
AnshuAlawa <alawapappu@gmail.com>
2023-05-10 16:32:18 +0530

    modify productSeller js file and index html file and also write some code in nav-bar html file

--- js/productSeller.js
+++ js/productSeller.js
@@ -1,1 +1,11 @@
-console.log('Wellcome');
+var app = angular.module('myApp', ['ui.router']);
+
+
+Directives('navBar','./Directives/nav-bar.html');
+
+
+
+
+function Directives(divName, tempName) {
+    app.directive(divName, function () { return { templateUrl: tempName } });
+}
